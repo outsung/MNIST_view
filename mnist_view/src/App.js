50bdb07c031a1fe00fa024d6a01032e690981f04
outsung <insung9546@gmail.com>
2020-01-09 22:42:59 +0900

    01.09

--- mnist_view/src/App.js
+++ mnist_view/src/App.js
@@ -1,181 +1,177 @@
 import React from 'react';
-import { create, all } from 'mathjs';
+import * as tf from '@tensorflow/tfjs';
+import * as tfvis from '@tensorflow/tfjs-vis';
 import './App.css';
 
-const math = create(all, { });
 
-function AND(x1, x2){
+async function getData() {
+  const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
+  const carsData = await carsDataReq.json();
 
-  let x = math.matrix([x1, x2]);
-  let w = math.matrix([0.5, 0.5]);
-  let b = -0.7;
+  const cleaned = carsData.map(car => ({
+    mpg: car.Miles_per_Gallon,
+    horsepower: car.Horsepower,
+  })).filter(car => (car.mpg != null && car.horsepower != null));
 
-  //tmp = sum(x * w) + b;
-  let tmp = math.chain(math.dotMultiply(x, w))
-                .sum()
-                .add(b)
-                .done();
-
-  if(tmp <= 0){
-    return 0;
-  }
-  else{
-    return 1;
-  }
-
+  return cleaned;
 }
 
-function NAND(x1, x2){
+function createModel() {
+  // 선형 모델 만들기
+  const model = tf.sequential();
 
-  let x = math.matrix([x1, x2]);
-  let w = math.matrix([-0.5, -0.5]);
-  let b = 0.7;
+  // 은닉층 1개 추가
+  model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
 
-  //tmp = sum(x * w) + b;
-  let tmp = math.chain(math.dotMultiply(x, w))
-                .sum()
-                .add(b)
-                .done();
+  // 출력층 추가
+  model.add(tf.layers.dense({units: 1}));
 
-  if(tmp <= 0){
-    return 0;
-  }
-  else{
-    return 1;
-  }
-
+  return model;
 }
 
+function convertToTensor(data){
+  return tf.tidy(() => {
+    // 데이터를 섞어준다.
+    tf.util.shuffle(data);
 
-function OR(x1, x2){
+    // 데이터를 텐서로 변환해준다.
+    const inputs = data.map(d => d.horsepower);
+    const labels = data.map(d => d.mpg);
 
-  let x = math.matrix([x1, x2]);
-  let w = math.matrix([0.5, 0.5]);
-  let b = -0.2;
+    const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+    const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
 
-  //tmp = sum(x * w) + b;
-  let tmp = math.chain(math.dotMultiply(x, w))
-                .sum()
-                .add(b)
-                .done();
+    // 0 ~ 1 범위만큼 데이터를 정규화 시킨다.
+    const inputMax = inputTensor.max();
+    const inputMin = inputTensor.min();
+    const labelMax = labelTensor.max();
+    const labelMin = labelTensor.min();
 
-  if(tmp <= 0){
-    return 0;
-  }
-  else{
-    return 1;
-  }
+    const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+    const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
 
+    return {
+      inputs: normalizedInputs,
+      labels: normalizedLabels,
+      // 최대, 최소 값도 넘겨줘서 나중에 재사용 가능하게 해준다.
+      inputMax,
+      inputMin,
+      labelMax,
+      labelMin,
+    }
+  });
 }
 
-function XOR(x1, x2){
+async function trainModel(model, inputs, labels){
+  // 훈련 준비 (모델 컴파일)
+  model.compile({
+    optimizer: tf.train.adam(),
+    loss: tf.losses.meanSquaredError,
+    mertics: ['mse'],
+  });
 
-  let s1 = NAND(x1, x2);
-  let s2 = OR(x1, x2);
+  const batchSize = 32;
+  const epochs = 50;
 
-  let y = AND(s1, s2);
-  return y;
-}
-
-
-
-// 계단 함수
-function step_function(x){
-  //let y = x > 0;
-  let y = math.larger(x, 0);
-  return math.number(y);
-}
-
-
-// 시그모이드
-function sigmoid(x){
-  // 1 / (1 + math.exp(-x))
+  // 훈련 시작
+  return await model.fit(inputs, labels, {
+    batchSize,
+    epochs,
+    shuffle: true,
+    callbacks: tfvis.show.fitCallbacks(
+      { name: 'Training Performance' },
+      ['loss', 'mse'],
+      { height: 200, callbacks: ['onEpochEnd'] }
+    )
+  });
   
-  let tmp = math.chain(math.dotMultiply(-1, x))
-                .exp()
-                .add(1)
-                .done();
+
+}
+
+function testModel(model, inputData, normalizationData){
+  const {inputMax, inputMin, labelMin, labelMax} = normalizationData;
+
+
+  //
+  const [xs, preds] = tf.tidy(() => {
+
+    const xs = tf.linspace(0, 1, 100);
+    const preds = model.predict(xs.reshape([100, 1]));
+
+    const nuNormXs = xs
+      .mul(inputMax.sub(inputMin))
+      .add(inputMin);
+    
+    const nuNormPreds = preds
+      .mul(labelMax.sub(labelMin))
+      .add(labelMin);
+      
+    // 정규화 하지 않은 데이터 (Un-normalize)
+    return [nuNormXs.dataSync(), nuNormPreds.dataSync()];
+  });
+
+  const predictedPoints = Array.from(xs).map((val, i)=>{
+    return {x: val, y: preds[i]}
+  });
+
+  const originalPoints = inputData.map(d => ({
+    x: d.horsepower, y: d.mpg,
+  }));
+
+  tfvis.render.scatterplot(
+    {name: 'Model predictions X Original Data'},
+    {values: [originalPoints, predictedPoints], series: ['original', 'predicted']},
+    {
+      xLabel: 'Horsepower',
+      yLabel: 'MPG',
+      height: 300
+    }
+  );
+}
+
+
+async function run(){
+  const data = await getData();
+  const values = data.map(d => ({
+    x: d.horsepower,
+    y: d.mpg,
+  }));
+
+  tfvis.render.scatterplot(
+    {name: 'Horsepower X MPG'},
+    {values}, 
+    {
+      xLabel: 'Horsepower',
+      yLabel: 'MPG',
+      height: 300
+    }
+  );
+
+  const model = createModel();
+  tfvis.show.modelSummary({name: 'Model Summary'}, model);
   
-  let y = math.dotDivide(1, tmp);
+  const tensorData = convertToTensor(data);
+  const {inputs, labels} = tensorData;
 
-  return y;
+  await trainModel(model, inputs, labels);
+  console.log('Done training');
+
+  testModel(model, data, tensorData);
+
 }
 
-// ReLU 함수
-function relu(x){
-  // math.maximum(0, x)
 
-  let zero = math.zeros(math.size(x));
-  const max = math.max;
-  let y = math.apply([zero, x], 0, max);
 
-  return y;
+
+function main() {
+  document.addEventListener('DOMContentLoaded', run);
 }
-
 
 
 function App() {
-
-  let res = 0;
-
-  console.log("---NAND---");
-  res = NAND(0, 1); // 0
-  console.log("0 && 1 => " + res);
-  res = NAND(0, 0); // 0
-  console.log("0 && 0 => " + res);
-  res = NAND(1, 1); // 1
-  console.log("1 && 1 => " + res);
-  res = NAND(1, 0); // 0
-  console.log("1 && 0 => " + res);
-
-
-  console.log("---OR---");
-  res = OR(0, 1); // 0
-  console.log("0 || 1 => " + res);
-  res = OR(0, 0); // 0
-  console.log("0 || 0 => " + res);
-  res = OR(1, 1); // 1
-  console.log("1 || 1 => " + res);
-  res = OR(1, 0); // 0
-  console.log("1 || 0 => " + res);
-
-
-  console.log("---XOR---");
-  res = XOR(0, 1); // 0
-  console.log("0 xor 1 => " + res);
-  res = XOR(0, 0); // 0
-  console.log("0 xor 0 => " + res);
-  res = XOR(1, 1); // 1
-  console.log("1 xor 1 => " + res);
-  res = XOR(1, 0); // 0
-  console.log("1 xor 0 => " + res);
-  
-
-  console.log("---step_function---");
-  res = step_function([1, 2, -2]);
-  console.log(res); // [1, 1, 0]
-
-
-  console.log("---sigmoid---");
-  res = sigmoid([-1.0, 1.0, 2.0]);
-  console.log(res); // [0.26894142, 0.73105858, 0.88079708]
-
-  
-  console.log("---relu---");
-  res = relu([-1.0, 1.0, 2.0]);
-  console.log(res); // [0, 1.0, 2.0]
-
-
-  let A,B;
-  console.log("---matrix---");
-  A = math.matrix([[1,3,4], [4,5,6]]);
-  B = math.matrix([[1,2], [3,4], [5,6]]);
-  res = math.dot(A, B);
-  console.log(res); // [[22, 28], [49, 64]]
-
-
+  main();
   return (
-    <div className="backgound">
+    <div className='backgound'>
       <h2>간단한 AND 제작 > {100}</h2>
     </div>
   );
